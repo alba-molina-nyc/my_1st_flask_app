322ca782ffe1081b160574e53ec4c16e02b10c8b
Username <albavmolinanyc@gmail.com>
2022-06-20 18:59:27 -0400

    POST and GET routes work

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -6,14 +6,6 @@ from datetime import datetime
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
-
-@app.route('/', methods=['POST', 'GET'])
-def index():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -23,3 +15,23 @@ class Todo(db.Model):
 
     def __repr__(self):
         return '<Task %r' % self.id
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except: 
+            return 'There was an issue adding your task'
+    else: 
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
