edee569fb5c549323eedc2c73d035a62e1efc766
Username <albavmolinanyc@gmail.com>
2022-06-20 18:35:02 -0400

    used terminal to create db todo using db object

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(300), nullable=False)
     completed = db.Column(db.Integer, default=0)
-    date_created = db.Column(db.Datetime, default=datetime.utcnow)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<Task %r' % self.id
