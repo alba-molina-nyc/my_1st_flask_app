bbdc521c7ba71f7d88626da9b065e6edf9eb7845
Username <albavmolinanyc@gmail.com>
2022-06-20 18:21:39 -0400

    sets up sql lite/sql alchemy

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
