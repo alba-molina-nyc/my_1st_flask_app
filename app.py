42b80784e4e4d93f41e05d37dd8af9c94271e9b9
Username <albavmolinanyc@gmail.com>
2022-06-20 19:01:08 -0400

    add delete route

--- app.py
+++ app.py
@@ -35,3 +35,14 @@ def index():
 if __name__ == '__main__':
     app.run(debug=True)
 
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem deleting that task'
