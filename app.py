aa2d1acf6c12b500f195751393fe4dce0adc8b14
Username <albavmolinanyc@gmail.com>
2022-06-20 19:35:33 -0400

    adds update

--- app.py
+++ app.py
@@ -44,5 +44,24 @@ def delete(id):
     except:
         return 'There was a problem deleting that task'
 
+
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.content = request.form['content']
+
+        try: 
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue updating your task'
+        
+    else: 
+        return render_template('update.html', task=task)
+
 if __name__ == '__main__':
     app.run(debug=True)
+
