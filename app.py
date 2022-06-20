4bec50631735e0065266738ea6be5accefe15066
Username <albavmolinanyc@gmail.com>
2022-06-20 18:16:03 -0400

    extends base.html, and css works!

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return 'Hello!'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
